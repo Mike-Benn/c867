625d80c25cf2e268ea10d784852b89e1ca364f1c
Mike-Benn <mbenn49@wgu.edu>
2024-04-06 01:38:21 -0400

    Adjust indentation of lines

--- student.h
+++ student.h
@@ -9,36 +9,36 @@ class Student {
 
     public:
 
-    Student(std::string studentID , std::string fName , std::string lName , std::string mail , int sAge , std::array<int, 3> , DegreeProgram program);
+        Student(std::string studentID , std::string fName , std::string lName , std::string mail , int sAge , std::array<int, 3> , DegreeProgram program);
 
-    
-    std::string getID() const;
-    std::string getFirstName() const;
-    std::string getLastName() const;
-    std::string getEmail() const;
-    int getAge() const;
-    std::array<int, 3> getDaysInCourse() const;
-    DegreeProgram getDegreeProgram() const;
+        
+        std::string getID() const;
+        std::string getFirstName() const;
+        std::string getLastName() const;
+        std::string getEmail() const;
+        int getAge() const;
+        std::array<int, 3> getDaysInCourse() const;
+        DegreeProgram getDegreeProgram() const;
 
-    void setID(std::string studentID);
-    void setFirstName(std::string fName);
-    void setLastName(std::string lName);
-    void setEmail(std::string mail);
-    void setAge(int sAge);
-    void setDaysInCourse(std::array<int, 3> days);
-    void setDegreeProgram(DegreeProgram program);
-    
-    void print();
+        void setID(std::string studentID);
+        void setFirstName(std::string fName);
+        void setLastName(std::string lName);
+        void setEmail(std::string mail);
+        void setAge(int sAge);
+        void setDaysInCourse(std::array<int, 3> days);
+        void setDegreeProgram(DegreeProgram program);
+        
+        void print();
 
     private:
 
-    std::string id;
-    std::string firstName;
-    std::string lastName;
-    std::string email;
-    int age;
-    std::array<int, 3> daysInCourse;
-    DegreeProgram degreeProgram;
+        std::string id;
+        std::string firstName;
+        std::string lastName;
+        std::string email;
+        int age;
+        std::array<int, 3> daysInCourse;
+        DegreeProgram degreeProgram;
 
 }
 
