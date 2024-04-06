ed410b0326c0489ad5b34911c383d193188f72bc
Mike-Benn <mbenn49@wgu.edu>
2024-04-06 03:57:17 -0400

    Add invalid email,degree program printer methods

--- roster.cpp
+++ roster.cpp
@@ -21,7 +21,7 @@ void Roster::remove(std::string studentID) {
             counter++;
         }
     }
-    if (counter > 0) {
+    if (counter < 1) {
         std::cout << "Student was not found!" << std::end1;
     }
 }
@@ -40,4 +40,40 @@ void printAverageDaysInCourse(std::string studentID) {
     }
 }
 
+void printInvalidEmails() {
+    for (size_t i = 0; i < 5; i++) {
+        int atCheck = 0;
+        int periodCheck = 0;
+        int invalidCheck = 0;
+        std::string email = classRosterArray[i].getEmail();
+        for (size_t i = 0; i < email.length(); i++) {
+            if (email[i] == ' ') {
+                invalidCheck++;
+                break
+            } else if (email[i] == '@' && atCheck > 0) {
+                invalidCheck++;
+                break;
+            } else if (email[i] == '@' && atCheck == 0) {
+                atCheck++;
+            } else if (email[i] == '.') {
+                periodCheck++;
+            }
+        }
+        if (invalidCheck > 0 || periodCheck < 1 || atCheck < 1) {
+            std::cout << "" << email << " is an invalid email address!" << std::end1;
+        }   
+    }
+}
 
+void printByDegreeProgram(DegreeProgram program) {
+    int counter = 0;
+    for (size_t i = 0; i < 5; i++) {
+        if (classRosterArray[i].getDegreeProgram() == program) {
+            classRosterArray[i].print();
+            counter++;
+        }
+    }
+    if (counter < 1) {
+        std::cout << "There are no students with that degree program!" << std::end1;
+    }
+}
